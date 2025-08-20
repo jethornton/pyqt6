0e7c1b6dbe70849d5a8b0114f2dbcc0a2168aaf7
John Thornton <dev@gnipsel.com>
2025-08-20 07:28:34 -0500

    work on keyboard jog
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- Event Filter/jog.py
+++ Event Filter/jog.py
@@ -11,31 +11,58 @@ class KeyboardJog(QObject):
 		super().__init__()
 	def eventFilter(self, obj, event):
 		#print(event.type())
-		if event.type() == QEvent.Type.KeyPress:
-			#print('KeyPress')
-			if event.key() == Qt.Key.Key_Up:
-				window.key_lb.setText('KeyPress Key_Up')
-				return True  # Consume the event
-			elif event.key() == Qt.Key.Key_Down:
-				window.key_lb.setText('KeyPress Key_Down')
-				return True  # Consume the event
-			elif event.key() == Qt.Key.Key_PageUp:
-				window.key_lb.setText('KeyPress Key_PageUp')
-				return True  # Consume the event
-			elif event.key() == Qt.Key.Key_PageDown:
-				window.key_lb.setText('KeyPress Key_PageDown')
-				return True  # Consume the event
-		elif event.type() == QEvent.Type.ShortcutOverride:
-			#print('ShortcutOverride')
-			if event.key() == Qt.Key.Key_Left:
-				window.key_lb.setText('ShortcutOverride Key_Left')
-				return True  # Consume the event, preventing QTabWidget from processing it
-			elif event.key() == Qt.Key.Key_Right:
-				window.key_lb.setText('ShortcutOverride Key_Right')
-				return True  # Consume the event, preventing QTabWidget from processing it
-		elif event.type() == QEvent.Type.KeyRelease:
-			window.key_lb.setText('KeyRelease')
-			return True  # Consume the event, preventing QTabWidget from processing it
+		# only handle the keys if keyboard jog is checked
+		if window.keyboard_jog_cb.isChecked():
+			if event.type() == QEvent.Type.KeyPress:
+				if not event.isAutoRepeat():
+					print(f'KeyPress {event.key()}')
+					if event.key() == Qt.Key.Key_Up:
+						window.key_lb.setText('KeyPress Key_Up')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_Down:
+						window.key_lb.setText('KeyPress Key_Down')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_PageUp:
+						window.key_lb.setText('KeyPress Key_PageUp')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_PageDown:
+						window.key_lb.setText('KeyPress Key_PageDown')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_Left:
+						window.key_lb.setText('ShortcutOverride Key_Left')
+						return True  # Consume the event, preventing QTabWidget from processing it
+					elif event.key() == Qt.Key.Key_Right:
+						window.key_lb.setText('ShortcutOverride Key_Right')
+						return True  # Consume the event, preventing QTabWidget from processing it
+				elif event.isAutoRepeat():
+					return True
+			elif event.type() == QEvent.Type.ShortcutOverride:
+				if not event.isAutoRepeat():
+					print(f'ShortcutOverride {event.key()}')
+					if event.key() == Qt.Key.Key_Up:
+						window.key_lb.setText('KeyPress Key_Up')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_Down:
+						window.key_lb.setText('KeyPress Key_Down')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_PageUp:
+						window.key_lb.setText('KeyPress Key_PageUp')
+						return True  # Consume the event
+					elif event.key() == Qt.Key.Key_PageDown:
+						window.key_lb.setText('KeyPress Key_PageDown')
+						return True  # Consume the event
+					if event.key() == Qt.Key.Key_Left:
+						window.key_lb.setText('ShortcutOverride Key_Left')
+						return True  # Consume the event, preventing QTabWidget from processing it
+					elif event.key() == Qt.Key.Key_Right:
+						window.key_lb.setText('ShortcutOverride Key_Right')
+						return True  # Consume the event, preventing QTabWidget from processing it
+				elif event.isAutoRepeat():
+					return True
+			elif event.type() == QEvent.Type.KeyRelease:
+				if not event.isAutoRepeat():
+					window.key_lb.setText('KeyRelease')
+					return True  # Consume the event, preventing QTabWidget from processing it
 
 		return super().eventFilter(obj, event)
 
@@ -49,6 +76,11 @@ class main(QMainWindow):
 
 		self.keyboard_jog = KeyboardJog(self)
 		app.installEventFilter(self.keyboard_jog)
+
+		app.aboutToQuit.connect(self.cleanup)
+
+	def cleanup(self):
+		app.removeEventFilter(self.keyboard_jog)
 
 app = QApplication(sys.argv)
 window = main()
